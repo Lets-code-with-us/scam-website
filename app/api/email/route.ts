f58b00135179b79a4d577d53cacb52b179c5e67a
Shu12388y <spl.sp999@gmail.com>
2024-01-27 23:13:29 +0530

    file changed

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -2,15 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 import { sendEmail } from "@/util/mailService";
 import { dbConnect } from "@/db/dbConfig";
 
-
 // dbConnect()
 export async function POST(request: NextRequest) {
-    try {
+  try {
     const requestBody = await request.json();
     await sendEmail(requestBody);
     return NextResponse.json({ message: "success" }, { status: 200 });
-  } catch (error: any) {
-    console.error('Error in POST route:', error);
+  } 
+  catch (error: any) {
+    console.error("Error in POST route:", error);
     return NextResponse.json({ error: error }, { status: 500 });
   }
 }
